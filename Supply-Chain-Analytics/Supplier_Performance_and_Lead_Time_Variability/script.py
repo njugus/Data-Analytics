4c729fd2e31a8729c91d0c2a6de8641d6ccfb233
njugus <kelvinkagwima091@gmail.com>
2026-06-10 01:13:13 -0700

    performs meric aggregations to calculate supplier performance score

--- Supply-Chain-Analytics/Supplier_Performance_and_Lead_Time_Variability/script.py
+++ Supply-Chain-Analytics/Supplier_Performance_and_Lead_Time_Variability/script.py
@@ -11,7 +11,30 @@ df['delivery_date'] = pd.to_datetime(df['delivery_date'], errors='coerce')
 # check for duplicate supplier records
 df.duplicated(subset=['order_id']).sum() 
 
-# ensure consistent category names or other column lables
-print(df.to_string())
+# transform the data
+# evaluate the performance of the suppliers using three metrics, lead_time, order_rate and deviation from the set delivery lead time
+# lead_time in days
+df['lead_time'] = (df['delivery_date'] - df['order_date']).dt.days
 
+# order-fill_rate
+df['order_fill_rate'] = ((df['quantity_delivered'] / df['quantity_ordered']) * 100).round(1)
 
+# filter by order_fill rate
+# filtered_df = df[df['order_fill_rate'] >= 100]
+
+# filter by supplier to get the supplier with the highest number of order fill rates across all medicines
+# dfx = filtered_df.groupby('supplier')['order_fill_rate'].count().reset_index()
+
+# perform aggregations
+# average lead time in days for each supplier across all medicines
+# avrage order fill rate for each supplier accross diffrent medicines
+# standard deviation of lead time - checks for variability or consistency(how spreac are the values from the average)
+# total orders made, total value delivered
+
+supplier_stats = df.groupby('supplier').agg(
+    avg_lead_time = ("lead_time", "mean"),
+    avg_fill_rate = ("order_fill_rate", "mean"),
+    lead_time_deviation = ("lead_time", "std")
+).reset_index()
+
+print(supplier_stats.to_string())
