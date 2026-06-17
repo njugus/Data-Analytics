ea09b04afdb67637a7172dc5df6052b9a3100382
njugus <kelvinkagwima091@gmail.com>
2026-06-17 02:07:36 -0700

    converts sign up date from string to datetime

--- Financial-Performance-Analytics/E-Commerce_Sales_Dataset/script.py
+++ Financial-Performance-Analytics/E-Commerce_Sales_Dataset/script.py
@@ -1,19 +1,22 @@
 import pandas as pd
 
 # load the csv data into the dataframe for manipulation purposes
-
-# change the str to date type
 customers = pd.read_csv('customers.csv')
-
 order_items = pd.read_csv("order_items.csv")
-# change the order date from str to datetime
 orders = pd.read_csv("orders.csv")
-
-# change the str date to datetime
 payments = pd.read_csv("payments.csv")
-
 products = pd.read_csv("products.csv")
+
+# change the str to datetimes
+# Remove leading/trailing whitespace
+customers['signup_date'] = customers['signup_date'].str.strip()
+customers['signup_date'] = pd.to_datetime(customers['signup_date'], format='%Y-%m-%d', errors='coerce')
+
+# orders['order_date'] = orders['order_date'].str.strip()
+orders['order_date'] = pd.to_datetime('order_date', format='%Y-%m-%d', errors='coerce')
+payments['payment_date'] = payments['payment_date'].str.strip()
+payments['payment_date'] = pd.to_datetime('payment_date', format='%Y-%m-%d',  errors='coerce')
 
 # get the summary info about the data in the dataframe
 # data type, null values present, total rows, total columns etc
-print(products.info())
+print(customers.info())
