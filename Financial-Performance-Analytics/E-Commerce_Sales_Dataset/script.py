5ff5de7a19497f1ed8396f7f1b8bdd73d22e3e79
njugus <kelvinkagwima091@gmail.com>
2026-06-18 00:06:00 -0700

    updates wrongky passed arguments format to the right one

--- Financial-Performance-Analytics/E-Commerce_Sales_Dataset/script.py
+++ Financial-Performance-Analytics/E-Commerce_Sales_Dataset/script.py
@@ -12,11 +12,13 @@ products = pd.read_csv("products.csv")
 customers['signup_date'] = customers['signup_date'].str.strip()
 customers['signup_date'] = pd.to_datetime(customers['signup_date'], format='%Y-%m-%d', errors='coerce')
 
-# orders['order_date'] = orders['order_date'].str.strip()
-orders['order_date'] = pd.to_datetime('order_date', format='%Y-%m-%d', errors='coerce')
+orders['order_date'] = orders['order_date'].str.strip()
+orders['order_date'] = pd.to_datetime(orders['order_date'], format='%Y-%m-%d', errors='coerce')
+
 payments['payment_date'] = payments['payment_date'].str.strip()
-payments['payment_date'] = pd.to_datetime('payment_date', format='%Y-%m-%d',  errors='coerce')
+payments['payment_date'] = pd.to_datetime(payments['payment_date'], format='%Y-%m-%d',  errors='coerce')
 
 # get the summary info about the data in the dataframe
 # data type, null values present, total rows, total columns etc
-print(customers.info())
+print(payments.info())
+
